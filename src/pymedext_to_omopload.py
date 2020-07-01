ce5b51e1bac91a7981cc0252781b18b5637098c4
willdgn <william.digan.er@gmail.com>
2020-07-01 19:08:30 +0200

    add function to load data into a docker omopdb

--- src/pymedext_to_omopload.py
+++ src/pymedext_to_omopload.py
@@ -5,7 +5,6 @@ from intervaltree import Interval,IntervalTree
 import argparse
 import json
 from datetime import datetime
-args = parser.parse_args()
 import psycopg2
 import psycopg2.extras
 from typing import Iterator, Dict, Any, Optional
@@ -21,8 +20,19 @@ logging.basicConfig(format='%(asctime)s -- %(name)s - %(levelname)s : %(message)
 parser = argparse.ArgumentParser()
 parser.add_argument("-i","--inputFolder", help="input annotationsFiles")
 
+args = parser.parse_args()
+DB_host =  "stagingdatabase"
+DB_name = "omopdb"
+DB_port = 5432
+#DB_user = "omopuser"
+#DB_password ="omopdb"
+DB_user = "postgres"
+DB_password ="postgres"
 
-logger.info("#####1")
+
+logger.info("start connection")
+omop_con = pymedext.OmopSource(DB_host, DB_name, DB_port, DB_user, DB_password)
+
 nlp_workflow="nlp_workflow_V1"
 filterType = ["sentence"]
 mypath = args.inputFolder
@@ -44,7 +54,10 @@ note_id =int(info_pat[2])#
 thisTime = datetime.strptime(info_pat[-1], '%Y-%m-%d')
 note_nlp_id = 1#"nlp_id"
 logger.info("############")
-annotations, dict_table_note, dict_table_person = pymedext.omop.buildNoteNlP(thisRoot, dict_note, note_id,note_nlp_id, nlp_workflow,thisTime,  filterType,True)
+annotations, dict_table_note, dict_table_person = pymedext.omop.buildNoteNlP(thisRoot, dict_note, note_id,note_nlp_id, nlp_workflow,thisTime,  filterType,False)
 logger.info(annotations)
 logger.info(dict_table_note)
 logger.info(dict_table_person)
+
+if annotations:
+    omop_con.saveToSource(dict_table_person, dict_table_note, annotations)
