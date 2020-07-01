91c017e932cb9d357adc1d4b94cace7a214eb422
willdgn <william.digan.er@gmail.com>
2020-07-01 17:53:44 +0200

    add the pymedext dockerfile and the build and run script

---
+++ bin/runInteractive.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -u
+
+function printgreen() {
+  printf "\\x1b[32m%s\\x1b[0m" "$1"
+}
+
+function printred() {
+  printf "\\x1b[31m%s\\x1b[0m" "$1"
+}
+
+function printskyblue() {
+  printf "\\x1b[36m%s\\x1b[0m" "$1"
+}
+
+function printlngreen() {
+  printf "\\x1b[32m%s\\x1b[0m\\n" "$1"
+}
+
+function printlnorange() {
+  printf "\\x1b[33m%s\\x1b[0m\\n" "$1"
+}
+
+function printlnred() {
+  printf "\\x1b[31m%s\\x1b[0m\\n" "$1"
+}
+
+function printlnskyblue() {
+  printf "\\x1b[36m%s\\x1b[0m\\n" "$1"
+}
+
+
+# base=$(realpath "$(dirname "$0")/..")
+base=$(realpath "$(dirname "$0")")
+
+
+printlnred $0
+
+printlnred $base
+
+docker run -it --rm  \
+ -v $base/..:/home/data pymedext-ehr bash
+ # --user 1000:1000
+printlnred "interacif mode is over"
